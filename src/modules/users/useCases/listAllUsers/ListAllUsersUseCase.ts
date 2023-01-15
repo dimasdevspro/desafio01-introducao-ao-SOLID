f177c8e331a4334de7f15ebee94d0393864d8ea8
Dimas Alves Pereira <dimasdevspro@gmail.com>
2023-01-15 00:29:56 -0300

    all tests passed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    
+    const user = this.usersRepository.findById(user_id);
+
+    if( !user.admin || !user ){
+      throw new Error("This user is not admin or not exists!")
+    }
+
+    return this.usersRepository.list();
   }
 }
 
